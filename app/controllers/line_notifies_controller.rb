6ec65ed968d45473df44379dfa1c9d18d0dcda74
nozayasu <84norin@gmail.com>
2017-11-17 11:43:30 +0900

    Add auth action to LineNotify

--- app/controllers/line_notifies_controller.rb
+++ app/controllers/line_notifies_controller.rb
@@ -1,14 +1,14 @@
 class LineNotifiesController < ApplicationController
     def auth
-        $uri = 'https://notify-bot.line.me/oauth/authorize' \
-            '?response_type=code' \
-            '&client_id=' + ENV["LINE_NOTIFY_CLIENT_ID"] \
-            '&redirect_uri=' + ENV["LINE_NOTIFY_REDIRECT_URI"] \
-            '&scope=notify' \
-            '&state=' + form_authenticity_token() \
+        uri = 'https://notify-bot.line.me/oauth/authorize' + \
+            '?response_type=code' + \
+            '&client_id=' + ENV["LINE_NOTIFY_CLIENT_ID"] + \
+            '&redirect_uri=' + ENV["LINE_NOTIFY_REDIRECT_URI"] + \
+            '&scope=notify' + \
+            '&state=' + form_authenticity_token() + \
             '&response_mode=form_post'
-        binding.pry
-        #return redirect_to($uri);
+
+        return redirect_to(uri);
     end
 
     def callback
