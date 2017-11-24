9083ee1b9d8f52f6ba98b604bd6388a0e4eb9ca8
nozayasu <84norin@gmail.com>
2017-11-24 15:38:45 +0900

    Add line bot callback

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "auth" => "line_notifies#auth"
   post "callback" => "line_notifies#callback"
   root to: "line_notifies#index"
+  post "bot/callback" => "line_bots#callback"
 end
